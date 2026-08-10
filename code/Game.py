eca40ce44f0953c0a96cbba3d6093f2af9d94519
Dominique <5119889@alunouninter.com>
2026-08-10 10:57:51 -0300

    Creating main loop Conneting Menu and Game classes Creating Menu Screen prototype Adding fonts

--- code/Game.py
+++ code/Game.py
@@ -2,28 +2,37 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.Const import SCREEN_WIDTH, SCREEN_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode(size=(600, 480))
+
+        pygame.mixer.music.load(
+            './asset/Galaxy Productions -Voyager cut.wav'
+        )
+        pygame.mixer.music.play(-1)
+
+        self.screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption("BeeCareful!")
+
         self.running = True
         self.score = None
         self.timer = None
+
         self.menu = Menu(self.screen)
 
     def start(self):
         while self.running:
-            self.menu.draw()
-            pygame.display.flip()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
-            self.screen.fill((180,220,255))
+            self.menu.draw()
+
             pygame.display.flip()
 
         pygame.quit()
--- code/Menu.py
+++ code/Menu.py
@@ -1,13 +1,118 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+
 
 class Menu:
     def __init__(self, screen):
         self.screen = screen
         self.state = None
 
+        # Fundo
+        self.surf = pygame.image.load('./asset/sky.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
+
+        # Fonte
+        self.font_title = pygame.font.Font(None, 48)
+        self.font_subtitle = pygame.font.Font(None, 22)
+        self.font_button = pygame.font.Font(None, 32)
+        self.font_commands = pygame.font.Font(None, 20)
+
+        # Cores
+        self.white = (255, 255, 255)
+        self.dark = (40, 60, 70)
+
+        # Botão
+        self.button_rect = pygame.Rect(250, 180, 100, 45)
+
     def draw(self, ):
-        pass
+        # Fundo
+        self.screen.blit(self.surf, self.rect)
+
+        # Título
+        title = self.font_title.render(
+            "BEECAREFUL!",
+            True,
+            self.dark
+        )
+
+        title_rect = title.get_rect(center=(300, 65))
+        self.screen.blit(title, title_rect)
+
+        # Subtítulos
+        subtitle1 = self.font_subtitle.render(
+            "Ajude a abelhinha a coletar pólen",
+            True,
+            self.dark
+        )
+
+        subtitle2 = self.font_subtitle.render(
+            "e salvar a colmeia!",
+            True,
+            self.dark
+        )
+
+        subtitle1_rect = subtitle1.get_rect(center=(300, 105))
+        subtitle2_rect = subtitle2.get_rect(center=(300, 125))
+
+        self.screen.blit(subtitle1, subtitle1_rect)
+        self.screen.blit(subtitle2, subtitle2_rect)
+
+        # Botão JOGAR
+        pygame.draw.rect(
+            self.screen,
+            self.white,
+            self.button_rect,
+            border_radius=8
+        )
+
+        button_text = self.font_button.render(
+            "JOGAR",
+            True,
+            self.dark
+        )
+
+        button_text_rect = button_text.get_rect(
+            center=self.button_rect.center
+        )
+
+        self.screen.blit(button_text, button_text_rect)
+
+        # Comandos
+        commands_title = self.font_commands.render(
+            "── COMANDOS ──",
+            True,
+            self.dark
+        )
+
+        commands_title_rect = commands_title.get_rect(
+            center=(300, 275)
+        )
+
+        self.screen.blit(commands_title, commands_title_rect)
+
+        commands_move = self.font_commands.render(
+            "W A S D / SETAS  -  Mover",
+            True,
+            self.dark
+        )
+
+        commands_pause = self.font_commands.render(
+            "ESC  -  Pausar",
+            True,
+            self.dark
+        )
+
+        move_rect = commands_move.get_rect(
+            center=(300, 305)
+        )
+
+        pause_rect = commands_pause.get_rect(
+            center=(300, 325)
+        )
+
+        self.screen.blit(commands_move, move_rect)
+        self.screen.blit(commands_pause, pause_rect)
 
     def startGame(self, ):
         pass
